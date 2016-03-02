390d7376420d3baaaa66e7be684b54fe9143f1a9
Rishi Bedi <rbedi100@gmail.com>
2016-03-02 01:02:12 -0800

    saving and loading model and transitions works

--- agent.py
+++ agent.py
@@ -14,20 +14,32 @@ class Agent(object):
 		self.sess = tf.Session()
 		self.transitions = TransitionTable()
 		self.tfSaver = tf.train.Saver()
-		if len(sys.argv) > 1:
+		if len(sys.argv) > 2:
 			path = sys.argv[1]
 			self.tfSaver.restore(self.sess, path)
 			print "RESTORED MODEL FROM PATH: %s" % path
+			f = open(sys.argv[2], 'r')
+			to_restore = pickle.load(f)
+			self.frame_count, self.transitions = to_restore['frame_count'], to_restore['transitions']
+			print "RESTORED TRANSITIONS FROM PATH %s to FRAME COUNT %d" % (path, self.frame_count)
 		else:
 			self.sess.run(tf.initialize_all_variables())
 
+	def save_all(self):
+		self.save_model()
+		print "Model saved at frame %d" % self.frame_count
+		self.save_transitions()
+		print "Transitions saved at frame %d" % self.frame_count
+
 	def save_model(self):
-		path = self.tfSaver.save(self.sess, "q-net", self.frame_count)
+		path = self.tfSaver.save(self.sess, "q-net")
 		print "SAVED MODEL TO PATH: %s" % path
 
 	def save_transitions(self):
-		pickle.dump(self.transitions, )
-		pass
+		path = "./transitions.pickle" 
+		f = open(path, 'w+')
+		to_dump = {'frame_count': self.frame_count, 'transitions': self.transitions}
+		pickle.dump(to_dump, f)
 
 	def epsilon(self):
 		return ((hp.FINAL_EXPLORATION - hp.INITIAL_EXPLORATION) / hp.FINAL_EXPLORATION_FRAME) * self.frame_count + hp.INITIAL_EXPLORATION
@@ -37,6 +49,10 @@ class Agent(object):
 		self.frame_count += 1
 
 		self.transitions.add_transition(image, terminal, action, reward, was_start)
+
+		if self.frame_count % hp.CHECKPOINT_FREQUENCY == 0:
+			self.save_all()
+
 		if self.frame_count < hp.REPLAY_START_SIZE:
 			return Action.random_action()
 
@@ -48,10 +64,6 @@ class Agent(object):
 		if self.frame_count % (hp.UPDATE_FREQUENCY * hp.TARGET_UPDATE_FREQUENCY) == 0:
 			self.sess.run(update_target)
 
-		if self.frame_count % hp.CHECKPOINT_FREQUENCY == 0:
-			self.save_model()
-
-		
 		if np.random.rand() < self.epsilon():
 			return Action.random_action()
 		img = self.transitions.get_recent()
