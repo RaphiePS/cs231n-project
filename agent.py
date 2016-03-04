a9b0d51eb21a9dfc6078305123a62d2539ec27ef
April Yu <yu.april.s@gmail.com>
2016-03-03 17:29:56 -0800

    save model to EBS

--- agent.py
+++ agent.py
@@ -34,7 +34,7 @@ class Agent(object):
 		print "Transitions saved at frame %d" % self.frame_count
 
 	def save_model(self):
-		path = self.tfSaver.save(self.sess, "./q-net")
+		path = self.tfSaver.save(self.sess, "/data/q-net")
 		print "SAVED MODEL TO PATH: %s" % path
 
 	def save_transitions(self):
